5095a6ae4160900abb230614ecd987405b3874a4
Lucas Monteiro <lucmont00@gmail.com>
2025-06-25 01:37:33 -0300

    Update generative_image.py

--- generative_image.py
+++ generative_image.py
@@ -1,24 +1,16 @@
-# -*- coding: utf-8 -*-
-
 from diffusers import StableDiffusionXLPipeline
 
-# Carrega o modelo
 pipeline = StableDiffusionXLPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0"
 )
 
-# Texto que descreve a imagem
-prompt = "A game girl in sideral space playing Minecraft."
+prompt = "Teacher, teaching artificial intelligence."
 
-# Tamanho da imagem
 width = 1024
 height = 1024
 
-# Gera a imagem
 image = pipeline(prompt, width=width, height=height).images[0]
 
-# Salva a imagem
 image.save("generated_image.png")
 
-# Mostra a imagem
 image.show()
